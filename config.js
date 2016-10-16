043749e6b8d8f2868d6bf01e6880898b1dda4d19
Tiago Garcia <tiago.garcia@macys.com>
2016-10-15 17:21:10 -0700

    adding node-event-emitter

--- config.js
+++ config.js
@@ -17,6 +17,7 @@ System.config({
     "babel": "npm:babel-core@5.8.38",
     "babel-runtime": "npm:babel-runtime@5.8.38",
     "core-js": "npm:core-js@1.2.7",
+    "node-event-emitter": "npm:node-event-emitter@0.0.1",
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.4.1"
     },
---
+++ jspm_packages/npm/node-event-emitter@0.0.1.js
@@ -0,0 +1,1 @@
+module.exports = require("npm:node-event-emitter@0.0.1/index.js");
